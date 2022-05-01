2e1e35f5e279b03286db93f317c4656a7367a28d
SeanPaulson <spausm1@gmail.com>
2022-05-01 16:28:01 -0600

    test commit by sean

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <nav>
+          <ul>
+            <li>image</li>
+            <li>SearchBar</li>
+            <li>profile</li>
+            <li>dropdown</li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
